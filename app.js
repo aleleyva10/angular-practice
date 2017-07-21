21220bded3ebe0035a5916db0881aa2e500da20d
Alessandro Leyva <josealeyva87@gmail.com>
2017-07-21 09:55:55 -0500

    New Changes

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 // requires
 var express = require('express');
 var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
 var index = require('./modules/routes/index');
 var tasks = require('./modules/routes/tasks');
 
 
 // uses
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded ({ extended:true } ) );
 app.use('/', index);
 app.use('/tasks', tasks);
 
@@ -17,4 +20,4 @@ var port = process.env.PORT || 3000;
 // spin up server
 app.listen(port, function() {
   console.log('Listening on:', port);
-}); // end spin up
+}); // end spin up server
